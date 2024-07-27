2486ef913c193777b6f7dc635250e15e66f622ed
Parthiv Majumdar <126563746+CodeWithParthiv@users.noreply.github.com>
2024-07-27 10:35:29 +0530

    Delete models.py

--- models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Users(models.Model):
-    fname = models.CharField(max_length=250)
-    lname = models.CharField(max_length=250)
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=250)
-    # phone = models.CharField(max_length=10)
